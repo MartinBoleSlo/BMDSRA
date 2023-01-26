9845e924c1bf9b7d888c5dfd612e82078acf49bb
soheili <majid.soheily@gmail.com>
2023-01-26 09:29:23 +0100

    Update the merging sequence files, add validating the sequence file before merging, and ability to add the label.

--- Codes/Preprocessing.py
+++ Codes/Preprocessing.py
@@ -1,4 +1,5 @@
 import glob
+import os.path
 import re
 import pandas as pd
 import tqdm
@@ -20,20 +21,26 @@ def validation(file):
     return valid
 
 
-def merging_sequence_files(in_directory, out_directory):
+def merging_sequence_files(in_directory, out_directory, labels):
     paths = glob.glob(in_directory + "*.fastq")
     outfile = open(out_directory, 'w')
+    count = 0
     for filename in tqdm.tqdm(paths):
-        with open(filename) as file:
-            lines = file.readlines()
-            identity = re.findall(r"@\w+", lines[0])[0]
-            identity = identity.replace("@", "") + ","
-            outfile.write(identity)
-            record = ""
-            for i in range(1, len(lines), 4):
-                record += lines[i].strip()
-            record += "\n"
-            outfile.write(record)
+        count += 1
+        if validation(filename):
+            with open(filename) as file:
+                lines = file.readlines()
+                identity = re.findall(r"@\w+", lines[0])[0]
+                identity = identity.replace("@", "") + ","
+                outfile.write(identity)
+                record = ""
+                for i in range(1, len(lines), 4):
+                    record += lines[i].strip()
+                record += "," + labels[count % len(labels)]
+                record += "\n"
+                outfile.write(record)
+        else:
+            print("The sequence file {0}-{1} is not valid".format(count, os.path.basename(filename)))
     outfile.close()
     df = pd.read_csv(out_directory, sep=",", header=None)
     df = df.sample(frac=1).reset_index(drop=True)
