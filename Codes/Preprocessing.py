88c40f59c10b77742c26d66053d019ec21d88a4b
soheili <majid.soheily@gmail.com>
2023-02-10 08:25:16 +0100

    Add, sampling function, for performing subsampling of the given fastq file.

--- Codes/Preprocessing.py
+++ Codes/Preprocessing.py
@@ -4,11 +4,13 @@ import re
 import pandas as pd
 import tqdm
 import re
+import random
+from random import randint
 
 
-def validation(file):
+def validation(path):
     valid = True
-    with open(file) as file:
+    with open(path) as file:
         for num, line in enumerate(file, start=1):
             line = line.replace('\n', '')
             if not line:
@@ -45,3 +47,38 @@ def merging_sequence_files(in_directory, out_directory, labels):
     df = pd.read_csv(out_directory, sep=",", header=None)
     df = df.sample(frac=1).reset_index(drop=True)
     df.to_csv(out_directory, index=False, header=None)
+
+
+def sampling(path, ns=300, ls=10, seed=7):
+    random.seed(seed)
+    random_points = set()
+    num_lines = 0
+    with open(path, 'r') as fp:
+        num_lines = sum(1 for line in fp)
+    num_lines = int(num_lines / 4)
+
+    if num_lines < ns * ls:
+        message = "The file is shorter than threshold ({0} < {1}).".format(num_lines, ns)
+        raise Exception(message)
+    elif num_lines < ns * ls * ls:
+        random_points = range(1, ns * ls, ls)
+    else:
+        while len(random_points) < ns:
+            random_points.add(randint(0, num_lines))
+
+    random_points = list(random_points)
+    random_points.sort()
+
+    random_indexes = []
+    for point in random_points:
+        win = range(point, point+ls, 1)
+        random_indexes.append(list(win))
+    random_indexes = sum(random_indexes, [])
+
+    seq = []
+    with open(path) as file:
+        for num, line in enumerate(file, start=1):
+            num = (num - 2) / 4 + 1
+            if num == int(num) and int(num) in random_indexes:
+                seq.append(line.replace('\n', ''))
+    return ''.join(seq)
