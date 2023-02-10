99073b631dcdbe52012e29fe4dbbe0b4f1941a36
soheili <majid.soheily@gmail.com>
2023-02-10 08:34:34 +0100

    Add, a unit test for sampling function.

--- Codes/test/ValidationTest.py
+++ Codes/test/ValidationTest.py
@@ -2,12 +2,12 @@ import unittest
 
 import pandas
 
-from Codes.Preprocessing import validation, merging_sequence_files
+from Codes.Preprocessing import validation, merging_sequence_files, sampling
 
 
 class MyTestCase(unittest.TestCase):
     def test_sequence_valid(self):
-        path = "../../resource/subsra/ERR209506.fastq"
+        path = "../../resource/2-subsra/ERR209506.fastq"
         res = validation(path)
         self.assertEqual(True, res)  # add assertion here
 
@@ -17,19 +17,25 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(False, res)  # add assertion here
 
     def test_merging_sequence_files(self):
-        in_dir = "../../resource/subsra/"
+        in_dir = "../../resource/2-subsra/"
         out_dir = in_dir + "merge.fasta"
-        lables = ['Isolated', 'Amplicon', 'Amplicon', 'Isolated', 'Metagenome', 'Metagenome', 'SAGs', 'Metagenome',
-                  'Amplicon', 'Amplicon', 'Metagenome', 'Amplicon', 'Metagenome', 'Metagenome', 'SAGs', 'Metagenome', 'Isolated',
+        labels = ['Isolated', 'Amplicon', 'Amplicon', 'Isolated', 'Metagenome', 'Metagenome', 'SAGs', 'Metagenome',
+                  'Amplicon', 'Amplicon', 'Metagenome', 'Amplicon', 'Metagenome', 'Metagenome', 'SAGs', 'Metagenome',
+                  'Isolated',
                   'Metagenome', 'Amplicon', 'Isolated', 'Isolated', 'Metagenome', 'SAGs', 'Amplicon', 'SAGs',
                   'Metagenome', 'Amplicon', 'Amplicon', 'Isolated', 'Metagenome', 'Isolated', 'Metagenome', 'SAGs',
                   'Isolated', 'SAGs', 'SAGs', 'Isolated', 'Amplicon', 'Amplicon', 'Isolated', 'Isolated']
-        merging_sequence_files(in_dir, out_dir, lables)
+        merging_sequence_files(in_dir, out_dir, labels)
         data = pandas.read_csv(out_dir, header=None)
         nr = data.shape[0]
         nc = data.shape[1]
-        self.assertEqual(nr, 40) # The SRR2545599.fastq is not valid
+        self.assertEqual(nr, 40)  # The SRR2545599.fastq is not valid
         self.assertEqual(nc, 3)
+
+    def test_sampling(self):
+        path = "../../resource/2-subsra/ERR867923.fastq"
+        res = sampling(path, ns=10, ls=2)
+        self.assertEqual(res[-10:], 'AAAACCTCCG')
 
 
 if __name__ == '__main__':
